eae720cb3d786e187c2dafd704f26f813de802f4
Craig Riley <c.riley@dsmdesign.co.uk>
2024-08-21 17:02:45 +0100

    Mode prop

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <header class="bg-teal-600 text-white py-2">
-    <div class="container flex justify-between">
-      <div>Vue TNS</div>
-      <div>Other</div>
-    </div>
-  </header>
-  <main>
-    <div class="container py-4">
-      <VueTns
-        ref="tnsRef"
-        pause-on-hover
-        centre
-        :responsive="{
-          xl: {
-            items: 2,
-            slideBy: 1,
-          },
-        }"
-      >
-        <div v-for="img in imagePaths" class="overflow-hidden">
-          <img :src="img" class="object-cover h-full" />
-        </div>
-      </VueTns>
-    </div>
-  </main>
+	<header class="bg-teal-600 text-white py-2">
+		<div class="container flex justify-between">
+			<div>Vue TNS</div>
+			<div>Other</div>
+		</div>
+	</header>
+	<main>
+		<div class="container py-4">
+			<VueTns
+				ref="tnsRef"
+				pause-on-hover
+				centre
+				:responsive="{
+					xl: {
+						items: 2,
+						slideBy: 1
+					}
+				}"
+			>
+				<div v-for="img in imagePaths" class="overflow-hidden">
+					<img :src="img" class="object-cover h-full" />
+				</div>
+			</VueTns>
+		</div>
+	</main>
 </template>
 
 <script setup>
@@ -31,13 +31,11 @@ import { ref, computed } from "vue";
 import { VueTns } from "vue-tns";
 
 const images = import.meta.glob("./images/*.jpg", { eager: true });
-const imagePaths = computed(() =>
-  Object.values(images).map((img) => img?.default ?? img)
-);
+const imagePaths = computed(() => Object.values(images).map((img) => img?.default ?? img));
 
 const tnsRef = ref(null);
 const onClick = () => {
-  console.log(tnsRef.value.getInfo());
+	console.log(tnsRef.value.getInfo());
 };
 
 // const images = [Image01, Image02, Image03];
